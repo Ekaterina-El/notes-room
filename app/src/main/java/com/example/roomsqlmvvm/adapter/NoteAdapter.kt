747037e776771ab80b823fc4805a814d3f585354
Ekaterina-El <hackerrussia@yandex.ru>
2022-02-27 13:14:47 +0500

    created adapter

--- app/src/main/java/com/example/roomsqlmvvm/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/roomsqlmvvm/adapter/NoteAdapter.kt
@@ -1,4 +1,35 @@
 package com.example.roomsqlmvvm.adapter
 
-class NoteAdapter {
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomsqlmvvm.R
+import com.example.roomsqlmvvm.model.NoteModel
+import kotlinx.android.synthetic.main.item_layout.view.*
+
+class NoteAdapter: RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+    inner class NoteViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    private var notes = mutableListOf<NoteModel>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+        return NoteViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.itemView.item_title.text = notes[position].title
+    }
+
+    override fun getItemCount(): Int = notes.size
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setNotes(list: MutableList<NoteModel>) {
+        notes = list
+        notifyDataSetChanged()
+    }
 }
