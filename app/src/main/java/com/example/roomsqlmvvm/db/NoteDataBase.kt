d75be62a99e7466fc0a028572c2fd4ed04cc612f
Ekaterina-El <hackerrussia@yandex.ru>
2022-02-27 13:37:12 +0500

    work with dao

--- app/src/main/java/com/example/roomsqlmvvm/db/NoteDataBase.kt
+++ app/src/main/java/com/example/roomsqlmvvm/db/NoteDataBase.kt
@@ -1,4 +1,28 @@
 package com.example.roomsqlmvvm.db
 
-class NoteDataBase {
+import android.content.Context
+import android.database.sqlite.SQLiteOpenHelper
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.roomsqlmvvm.db.dao.NoteDao
+import com.example.roomsqlmvvm.model.NoteModel
+
+@Database(entities = [NoteModel::class], version = 1)
+abstract class NoteDataBase: RoomDatabase() {
+    abstract fun getNoteDao(): NoteDao
+
+    companion object {
+        private var database: NoteDataBase ?= null
+
+        @Synchronized
+        fun getInstance(context: Context): NoteDataBase {
+            return if (database == null) {
+                database = Room.databaseBuilder(context, NoteDataBase::class.java, "db").build()
+                database as NoteDataBase
+            } else {
+                database as NoteDataBase
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/roomsqlmvvm/db/repository/NoteRepository.kt
+++ app/src/main/java/com/example/roomsqlmvvm/db/repository/NoteRepository.kt
@@ -1,4 +1,13 @@
 package com.example.roomsqlmvvm.db.repository
 
+import androidx.lifecycle.LiveData
+import com.example.roomsqlmvvm.model.NoteModel
+
+
+// "Список" функций и переменных для работы с БД
 interface NoteRepository {
+    val allNotes: LiveData<List<NoteModel>>
+
+    suspend fun insertNote(noteModel: NoteModel, onSuccess: () -> Unit)
+    suspend fun deleteNote(noteModel: NoteModel, onSuccess: () -> Unit)
 }
